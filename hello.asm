7d485523ec409630e4a70a6d32f3005561ab4992
Isvane <277444536+Isvane@users.noreply.github.com>
2026-07-26 01:16:17 +0700

    feat: add user input reading

--- hello.asm
+++ hello.asm
@@ -2,6 +2,9 @@ section .data
     msg db "Hello, World!", 10
     msg_len equ $ - msg
 
+section .bss
+    input: resb 64
+
 section .text
     global _start
 
@@ -12,6 +15,18 @@ _start:
     mov rdx, msg_len
     syscall
 
-    mov rax, 60
+    mov rax, 0
     mov rdi, 0
+    mov rsi, input
+    mov rdx, 64
+    syscall
+
+    mov rdx, rax
+    mov rsi, input
+    mov rax, 1
+    mov rdi, 1
+    syscall
+
+    mov rdi, 42
+    mov rax, 60
     syscall

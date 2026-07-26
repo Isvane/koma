49ea1feb6cde210866a447a7b38abaa489bafe14
Isvane <277444536+Isvane@users.noreply.github.com>
2026-07-26 09:54:22 +0700

    feat: implement basic cmp and conditional branching

--- src/hello.asm
+++ src/hello.asm
@@ -1,6 +1,10 @@
 section .data
     msg db "Hello, World!", 10
     msg_len equ $ - msg
+    yes db "Yes, it is 100!", 10
+    yes_len equ $ - yes
+    no db "No, it isn't 100!", 10
+    no_len equ $ - no
 
 section .bss
     input: resb 64
@@ -27,6 +31,27 @@ _start:
     mov rdi, 1
     syscall
 
+    mov rax, 100
+    cmp rax, 100
+    je is_equal
+
+not_equal:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, no
+    mov rdx, no_len
+    syscall
+
+    jmp exit_program
+
+is_equal:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, yes
+    mov rdx, yes_len
+    syscall
+
+exit_program:
     mov rdi, 42
     mov rax, 60
     syscall

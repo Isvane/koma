48bf953d9c89dd8112045d0790818e9aba6c0454
Isvane <277444536+Isvane@users.noreply.github.com>
2026-07-27 23:06:34 +0700

    feat(server): add POST request

--- src/server.asm
+++ src/server.asm
@@ -1,10 +1,17 @@
 section .data
     http_response db "HTTP/1.1 200 OK", 13, 10
                   db "Content-Type: text/html", 13, 10
-                  db "Content-Length: 28", 13, 10
+                  db "Content-Length: 25", 13, 10
                   db 13, 10
                   db "<h1>Isvane was here!</h1>"
     response_len equ $-http_response
+    http_post db "HTTP/1.1 200 OK", 13, 10
+              db "Content-Type: text/plain", 13, 10
+              db "Host: localhost: 8080", 13, 10
+              db "Content-Length: 14", 13, 10
+              db 13, 10
+              db "Goodbye World!"
+    post_len equ $-http_post
     serv_addr:
         dw 2
         db 0x26, 0xA0
@@ -12,7 +19,7 @@ section .data
         times 8 db 0
 
 section .bss
-    buffer resb 1024
+    buffer resb 8192
 
 section .text
     global _start
@@ -52,17 +59,30 @@ accept_loop:
     mov rax, 0
     mov rdi, r13
     mov rsi, buffer
-    mov rdx, 1024
+    mov rdx, 8192
     syscall
 
-    ; Write
-    mov rax, 1
-    mov rdi, r13
+    test rax, rax
+    jle close_conn
+
+    cmp byte [buffer], 'P'
+    je serve_post
+
+serve_get:
     mov rsi, http_response
     mov rdx, response_len
+    jmp do_write
+
+serve_post:
+    mov rsi, http_post
+    mov rdx, post_len
+
+do_write:
+    mov rax, 1
+    mov rdi, r13
     syscall
 
-    ; Close
+close_conn:
     mov rax, 3
     mov rdi, r13
     syscall
